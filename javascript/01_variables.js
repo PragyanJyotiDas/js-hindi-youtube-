45330c13c5dfca71c4238d839c7ecaecfe0f2b48
Pragyan Jyoti Das <ee240002055@iiti.ac.in>
2025-08-14 14:46:20 +0000

    commit

---
+++ javascript/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=2456
+let accountEmail="pragyan@gmail.com"
+var accountPassword="1234";
+accountCity = "dibrugarh"
+let accountState
+
+/*everything others can be changed
+never uuse the var to declare the variables 
+
+*/
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
