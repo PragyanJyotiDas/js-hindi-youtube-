4483a4cb217ea48057f227d6c6b834613899b600
Pragyan Jyoti Das <ee240002055@iiti.ac.in>
2025-08-15 18:18:44 +0000

    commit

---
+++ javascript/07_dateinJs.js
@@ -0,0 +1,17 @@
+//date
+let myDate =new Date()
+ console.log(myDate.toString());
+// console.log(myDate.toDateString())
+//console.log(myDate.toLocaleString())
+
+
+ let myCreatedDate = new Date("01-12-2023")
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString())
+
+
+let myTimeStamp = Date.now()
+
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
---
+++ javascript/nums_and_dates.js
@@ -0,0 +1,21 @@
+const score =400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString())
+
+console.log(balance.toString().length);
+
+const otherNumber  =123.2583
+
+console.log(otherNumber.toPrecision(6));
+
+
+//---------------------Math------------------
+
+
+console.log(Math);
+console.log(Math.abs(-3))
