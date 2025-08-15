f811127544869a6fc44556f69ff21f7f4dc736a2
Pragyan Jyoti Das <ee240002055@iiti.ac.in>
2025-08-15 09:27:17 +0000

    commit

---
+++ javascript/04_comparisons.js
@@ -0,0 +1,15 @@
+console.log(2>1)
+console.log ("2">1)
+
+console.log("part 2 ")
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+console.log("part 3")
+
+console.log(undefined==0)
+console.log(undefined>0)
+
+console.log("part 4")
+console.log("2"===1)//value and datatype 
