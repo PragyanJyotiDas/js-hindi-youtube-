4c8254028097fc76369a1c3002ff9cd596531e57
Pragyan Jyoti Das <ee240002055@iiti.ac.in>
2025-08-15 09:02:46 +0000

    commit

---
+++ javascript/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score) 
+console.log(typeof(valueInNumber));
+console.log(valueInNumber)
+
+//score="abc"   output is NaN->not a number
+//true =>1 ; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//""=>false 
+//"pragyan" =>true 
+
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
