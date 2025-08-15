310185c03afe10cee17f62a8bb8ecd1dcdcce8a1
Pragyan Jyoti Das <ee240002055@iiti.ac.in>
2025-08-15 17:52:15 +0000

    commit =

---
+++ javascript/05_string.js
@@ -0,0 +1,14 @@
+const name="pragyan"
+const repoCount = 50
+
+//console.log(name + repoCount+"Value");
+
+console.log('hello my name is ${name} and my repo count is ${repoCount}')
+
+const gameName = new String('pragyan')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+  
+
